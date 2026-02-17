80bcf63a65ce466746165b13230b1c95885a818a
AnvitaManne <anvita.manne@gmail.com>
2026-02-17 12:31:17 +0530

    Update

--- scripts/step3_find_studies.py
+++ scripts/step3_find_studies.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 ORTHANC = "http://localhost:8042"
+# Note: This UUID is the internal Orthanc ID for the patient
 PATIENT_UUID = "e193a01e-cf8d30ad-0affefd3-32ce934e-32ffce72"
 
 # Get patient info
@@ -12,3 +13,4 @@ studies = patient_info.get("Studies", [])
 print("Studies for this patient:")
 print(studies)
 print("Total:", len(studies))
+
